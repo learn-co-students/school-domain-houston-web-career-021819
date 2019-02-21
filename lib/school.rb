d318eb6c5c9bf97ee8fbd3bba6d8893ac926b507
moranja <jeffadammoran@gmail.com>
2019-02-20 21:54:37 -0600

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  attr_reader :roster
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.map do |grade, name_array|
+      sorted_roster[grade] = name_array.sort
+    end
+    return sorted_roster
+  end
+
+
+end
